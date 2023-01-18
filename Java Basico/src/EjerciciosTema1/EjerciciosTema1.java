dd4022efe51d3870b7574ea8a128bdf0f6dbced8
aleh-g <alejandroh.gallardo@gmail.com>
2023-01-18 19:34:02 -0300

    Entrega ejercicios tema 1

---
+++ Java Basico/src/EjerciciosTema1/EjerciciosTema1.java
@@ -0,0 +1,32 @@
+package EjerciciosTema1;
+
+public class EjerciciosTema1 {
+    public static void main(String[] args) {
+        //Enteros
+        byte var1 = 1;
+        System.out.println(var1);
+        short var2 = 12;
+        System.out.println(var2);
+        int var3 = 44;
+        System.out.println(var3);
+        long var4 = 13124;
+        System.out.println(var4);
+
+        //Flotantes
+        float var5 = 44.5f;
+        System.out.println(var5);
+        double var6 = 43.0254564d;
+        System.out.println(var6);
+
+        //Booleanos
+        boolean var7 = true;
+        System.out.println(var7);
+
+        //Texto
+        char var8 = 'a';
+        System.out.println(var8);
+        String var9 = "casa";
+        System.out.println(var9);
+
+    }
+}
